f5761d5ab0cbc1217c72a2f8fba389899e3253c5
rlau4 <p.lau.004@gmail.com>
2019-11-05 09:00:30 -0500

    updated font

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('app/public'));
 
-
-
 require("./app/routing/htmlRoutes")(app);
-
-
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
